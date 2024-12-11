8a50ed7e102ea4d8347bbea27e8fad4f7863b38a
Shaheen Ks <ks.shaheen@gmail.com>
2024-12-11 18:19:53 +0530

    Materialize CSS + API proxy added

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const M: any;
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+
+export class HomeComponent implements OnInit {
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    M.AutoInit()
+  }
+}
